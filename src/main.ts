2392332ad0a501a06325417ef888de0b36f14d67
Mohim Thakre <108913531+Mo-him@users.noreply.github.com>
2026-02-10 10:39:21 +0530

    Set default DNS result order to ipv4first

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
